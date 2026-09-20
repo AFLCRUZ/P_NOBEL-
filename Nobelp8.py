0109600b90389a5f08d8c29eb98f31148cd25f8f
AFLCRUZ <cruzlaura320@gmail.com>
2026-09-20 09:20:45 -0600

    Add files via upload

---
+++ Nobelp8.py
@@ -0,0 +1,53 @@
+import numpy as np
+import streamlit as st
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+st.write(''' # Nobel Prize Category Prediction ''')
+st.image("Nobel.png", caption="It was created by the Swedish inventor Alfred Nobel through his will in 1895.")
+
+st.header('Text')
+
+def user_input_features():
+  # Entrada
+  texto = st.text_input("Enter the text to evaluate")
+
+  user_input_data = {'Text': texto}
+
+  features = pd.DataFrame(user_input_data, index=[0])
+
+  return features
+
+df = user_input_features()
+
+nobel =  pd.read_csv('df_final.csv', encoding='latin-1')
+X = nobel.Text
+y = nobel.Label
+
+vect = CountVectorizer()
+X_dtm = vect.fit_transform(X)
+
+nb = MultinomialNB()
+nb.fit(X_dtm, y)
+
+df_dtm = vect.transform(df['Text'])
+prediction = nb.predict(df_dtm)
+
+#{'physics':0, 'medicine':1, 'peace':2, 'literature':3, 'chemistry':4, 'economics':5}
+#'Physics', 'Medicine', 'Peace', 'Literature', 'Chemistry', 'Economics'
+st.subheader('Prediction')
+if prediction == 0:
+  st.write('Physics')
+elif prediction == 1:
+  st.write('Medicine')
+elif prediction == 2:
+  st.write('Peace')
+elif prediction == 3:
+  st.write('Literature')
+elif prediction == 4:
+  st.write('Chemistry')
+elif prediction == 5:
+  st.write('Economics')
+else:
+  st.write('Sin predicción')
